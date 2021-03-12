24a423e8d6967ebdf414d667f14b474ac057e237
akmarzhan1 <47840436+akmarzhan1@users.noreply.github.com>
2021-03-13 05:27:55 +0600

    refactor: prettier

--- kanban/models.py
+++ kanban/models.py
@@ -3,8 +3,6 @@ from flask_login import UserMixin
 from datetime import datetime
 
 # user database
-
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True, nullable=False)
@@ -13,8 +11,6 @@ class User(db.Model, UserMixin):
     todos = db.relationship('Todo', backref="creator", lazy=True)
 
 # the original database specification for keeping track of the tasks
-
-
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
@@ -24,7 +20,6 @@ class Todo(db.Model):
     date_posted = db.Column(db.DateTime, default=datetime.utcnow)
     deadline = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
 
 @login_manager.user_loader
 def load_user(user_id):

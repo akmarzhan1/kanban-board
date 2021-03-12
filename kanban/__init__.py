fcfc5c7bfec2dee7795eb6a343d0b2aad05fce49
akmarzhan1 <47840436+akmarzhan1@users.noreply.github.com>
2021-03-13 05:27:15 +0600

    refactor: prettier

--- kanban/__init__.py
+++ kanban/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
 app = Flask(__name__, instance_relative_config=True)
 app.config['SECRET_KEY'] = 'secretkeynoonewillknow'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
